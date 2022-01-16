e9cc5daf5354cd3dc52d35e7d9d1b24cce923cfd
Alex-dqw <alexdqw2016@gmail.com>
2022-01-16 21:24:33 +0200

    'ReactPropsExample'

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-
-const user = {
-  firstName: 'Alex',
-  lastName: 'Dqw'
+const App = (props) => {
+  return <>
+    <p>Hello, {props.name}</p>
+  </>;
 };
 
-const element = (
-  <h1>
-    Hello, {formatName(user)}!
-  </h1>
-);
-
 ReactDOM.render(
-  element,
-  document.getElementById('root')
+    <App name={`Alex`} />,
+    document.getElementById(`root`)
 );
